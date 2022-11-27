aa9ae2501164f17fe36307669d298fbb380dc593
Ivan Demenkov <ariastation@ua.fm>
2022-11-27 18:37:50 +0200

    added some users

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/info", (req, res, next) => {
-    res.json({ msg: 'there an info block', error: false })
+const Users = require('../users');
+const UsersList = require('../userslist');
+
+router.get('/info', (req, res) => {
+    res.json({ msg: 'There is just an info block', error: false })
 });
+
+router.get('/users', (req, res) => {
+    res.json(UsersList);
+});
+
+router.get('/user/:id', (req, res) => {
+    const id = +req.params?.id || 0;
+
+    if (id < 10001 || id > 10010) {
+        res.json({ error: true, msg: 'User ID should be in range from 10001 to 10010!' });
+    } else {
+        res.json(Users.filter(user => user.id === id)[0]);
+    }
+})
 
 module.exports = router;
---
+++ users.js
@@ -0,0 +1,56 @@
+module.exports = [
+    {
+        id: 10001,
+        username: "jonnyd",
+        role: "admin",
+        first_name: "Jonny",
+        last_name: "Datagram",
+        creation_date: 1606900200000,
+        last_visit_date: 1668199597829
+    },
+    {
+        id: 10002,
+        username: "deivan",
+        role: "admin",
+        first_name: "Evan",
+        last_name: "D'One",
+        creation_date: 1606900300000,
+        last_visit_date: 1668200597829
+    },
+    {
+        id: 10003,
+        username: "kennym",
+        role: "user",
+        first_name: "Kenny",
+        last_name: "McCormick",
+        creation_date: 1606900400000,
+        last_visit_date: 1668201597829
+    },
+    {
+        id: 10004,
+        username: "stanm",
+        role: "user",
+        first_name: "Stanley",
+        last_name: "Marsh",
+        creation_date: 1606900500000,
+        last_visit_date: 1668204597829
+    },
+    {
+        id: 10005,
+        username: "vinnip",
+        role: "staff",
+        first_name: "Winnie",
+        last_name: "the Pooh",
+        creation_date: 1606900600000,
+        last_visit_date: 1668205597829
+    },
+    {
+        id: 10006,
+        username: "tomcat",
+        role: "staff",
+        first_name: "Thomas",
+        last_name: "Cat Sr.",
+        creation_date: 1606900700000,
+        last_visit_date: 1668206597820
+    }
+];
