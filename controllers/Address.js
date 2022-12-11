6cdf5762e8969cabfb5e33f85bca5981c5adc167
Ivan Demenkov <ariastation@ua.fm>
2022-12-11 21:06:37 +0200

    added Addresses and Payments routes

--- controllers/Address.js
+++ controllers/Address.js
@@ -7,7 +7,7 @@ module.exports = {
     if (id < 10001 || id > 10010) {
       res.status(400).json({ error: true, msg: 'User ID should be in range from 10001 to 10010!' });
     } else {
-      res.json([]);
+      res.json(Addresses.filter(adr => adr.userId === id));
     }
   },
   new: (req, res) => {
@@ -16,16 +16,27 @@ module.exports = {
     if (id < 10001 || id > 10010) {
       res.status(400).json({ error: true, msg: 'User ID should be in range from 10001 to 10010!' });
     } else {
-      res.json({});
+      res.json({
+        id: 20021,
+        userId: id,
+        type: req.params?.type || 'shipping',
+        country: req.params?.country || 'Neverland',
+        city: req.params?.city || 'Bigrouter',
+        address: req.params?.address || '255 Dev Null st.',
+        phone: req.params?.phone || '+127001'
+      });
     }
   },
   delete: (req, res) => {
     const id = +req.params?.id || 0;
 
-    if (id < 20001 || id > 20010) {
+    if (id < 20001 || id > 20020) {
       res.status(400).json({ error: true, msg: 'Address ID should be in range from 20001 to 20010!' });
     } else {
-      res.json({});
+      res.json({
+        error: true,
+        msg: `Address ${id} was deleted successfully`,
+      });
     }
   }
 };
--- controllers/Payments.js
+++ controllers/Payments.js
@@ -1,3 +1,6 @@
+const Payments = require('./PaymentsList');
+let payCounter = 30030;
+
 module.exports = {
   get: (req, res) => {
     const id = +req.params?.id || 0;
@@ -5,7 +8,7 @@ module.exports = {
     if (id < 10001 || id > 10010) {
       res.status(400).json({ error: true, msg: 'User ID should be in range from 10001 to 10010!' });
     } else {
-      res.json([]);
+      res.json(Payments.filter(pay => pay.userId === id));
     }
   },
   new: (req, res) => {
@@ -14,16 +17,26 @@ module.exports = {
     if (id < 10001 || id > 10010) {
       res.status(400).json({ error: true, msg: 'User ID should be in range from 10001 to 10010!' });
     } else {
-      res.json({});
+      res.json({
+        id: payCounter++,
+        userId: id,
+        type: "mcard",
+        card: "51****9999",
+        paymentId: "33bf62e09a05302506f3c846e827acbf642d57f3ae3c302028d50e33c4c7084e",
+        expires: "12/23",
+      });
     }
   },
   delete: (req, res) => {
     const id = +req.params?.id || 0;
 
-    if (id < 30001 || id > 30010) {
-      res.status(400).json({ error: true, msg: 'Payment ID should be in range from 30001 to 30010!' });
+    if (id < 30001 || id > 31000) {
+      res.status(400).json({ error: true, msg: 'Payment ID should be in range from 30001 to 31000!' });
     } else {
-      res.json({});
+      res.json({
+        error: true,
+        msg: `The Payment Method ${id} was deleted successfully`,
+      });
     }
   }
 };
